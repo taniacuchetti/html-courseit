be24c0b8b56fe70e4f1fdd9891085af169ea9873
Tania <tania.cuchetti@gmail.com>
2020-05-17 20:38:48 -0300

    update

--- javascript/borrar-inputs/script.js
+++ javascript/borrar-inputs/script.js
@@ -7,3 +7,30 @@ const clean = () => {
      input.value = null;
  }) 
 }
+
+
+
+  
+  function ejemplo() {
+    alert("Esto es un evento de click");
+  }
+  
+  function onBoxClick() {
+    console.log("La caja fue cliqueada");
+  }
+  
+  function onBoxEnter() {
+    console.log("El mouse entró a la caja");
+  
+    document.querySelector(".box").classList.add("over")
+  }
+  
+  function onBoxOut() {
+    console.log("El mouse salió de la caja");
+  
+    document.querySelector(".box").classList.remove("over")
+  }
+  
+  function onBoxMove() {
+    console.log("EL MOUSE SE MOVIOOOOOOO");
+  }
